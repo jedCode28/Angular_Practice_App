3fdbca2ef1a0e66ca58c49ab34cc72e4b4fadd2c
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-06 16:20:03 -0600

    app done

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: Array<any> =[];
 
-  ngOnInit(): void {
+  constructor(private _http: HttpService) { }
+
+  ngOnInit():void {
+    this._http.getBeer().subscribe(data => {
+      this.brews = data as Array<Object>;
+      console.log(this.brews);
+    }); 
   }
 
 }
